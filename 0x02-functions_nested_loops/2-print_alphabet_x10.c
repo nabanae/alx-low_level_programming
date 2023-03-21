a6083e8ba18f411a8a2503d09cfd9973e83873ca
nabanae <alhanlala@gmail.com>
2023-03-21 09:39:16 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - prints all alphabets in lowercase 10 times
+ * print_alphabet_x10 - prints all alphabets in lowercase 10 times
  * Return: On success 1.
  */
 void print_alphabet_x10(void)

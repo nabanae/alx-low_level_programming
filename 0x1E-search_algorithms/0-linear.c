0ccf8f080d656c91eb0207460a4096dcb5211847
nabanae <alhanlala@gmail.com>
2023-10-10 06:20:03 -0700

    plz

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,17 +14,28 @@
   */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	int i = 0, start = 0, end = (int)size - 1, mid = 0;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
-
-	for (i = 0; i < size; i++)
+	while (end >= start)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("Searching in array: ");
+		for (i = start; i <= end; i++)
+		{
+			printf("%i", array[i]);
+			if (i != end)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		mid = (start + end) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (value < array[mid])
+			end = mid - 1;
+		else
+			start = mid + 1;
 	}
-
 	return (-1);
 }
